62f1f6d3197430e8491b175e647b0356df3b49f0
emazack <em.zaccaria@gmail.com>
2022-10-10 18:09:59 +0200

    Finish the exercise

--- README.md
+++ README.md
@@ -7,4 +7,3 @@ Learning about to Work with hooks, lists of elements, create a local state (useS
 - invoke a function from JSX passing parameters
 - display dynamic images
 - apply dynamic styles
-
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+interface City {
+  id: number;
+  name: string;
+}
+
+const CITIES: City[] = [
+  { id: 1, name: 'Trieste' },
+  { id: 2, name: 'Milano' },
+  { id: 3, name: 'Rome' },
+];
+
 function App() {
+  const [currentCity, setCurrentCity] = useState<City | null>(null);
+  const url = 'https://www.mapquestapi.com/staticmap/v5/map?key=Go3ZWai1i4nd2o7kBuAUs4y7pnpjXdZn&size=200,100&center=' + currentCity?.name;
+
+  function viewMap(city: City) {
+    setCurrentCity(city);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        CITIES.map(city => {
+          const activeStyle = { color: city.id === currentCity?.id ? 'red' : 'black' }
+          return <button key={city.id} style={activeStyle} onClick={() => viewMap(city)}>{city.name}</button>
+        })
+      }
+        <hr/>
+        {currentCity?.name}
+        <div className='image-container'>
+          {currentCity && <img width={200} height={100} src={url} alt=""/>}
+        </div>
     </div>
   );
 }
